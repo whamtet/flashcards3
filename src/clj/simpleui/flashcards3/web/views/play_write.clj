da515598d3c3ad1503e3bd30bab25b67a368515e
Matthew Molloy <whamtet@gmail.com>
2026-03-17 00:02:37 +0700

    try new grid approach

--- src/clj/simpleui/flashcards3/web/views/play_write.clj
+++ src/clj/simpleui/flashcards3/web/views/play_write.clj
@@ -12,16 +12,34 @@
     (format "../../api/local/%s" x)))
 
 (defn- inc-mod [x]
-  (mod (inc x) 5))
+  (mod (inc x) 4))
 (defn- font-size [x]
   (str (max 0.5 (dec x)) "em"))
 
-[:div.grid-rows-2.grid-cols-2]
+[:div.grid.grid-rows-1.flex-1.min-h-0]
+[:div.grid-cols-1]
+[:div.grid-cols-2]
 [:div.grid-cols-3]
 [:div.grid-cols-4]
 [:div.grid-cols-5]
 [:div.grid-cols-6]
 [:div.grid-cols-7]
+(defn- half-column [enlargement slides]
+  [:div.flex.flex-col {:style {:height "50vh"}}
+   [:div {:class (format "grid grid-rows-1 grid-cols-%s flex-1 min-h-0" (count slides))}
+    (for [[src2 src] (map first slides)]
+      [:img {:class "max-h-full w-full object-contain"
+             :src (get-src src)
+             :src2 (get-src src2)
+             :onerror "fixSrc(event.target)"
+             :hx-get "panel"
+             :hx-vals {:enlargement (inc-mod enlargement)}}])]
+   (when (pos? enlargement)
+     [:div {:class (format "grid grid-rows-1 grid-cols-%s" (count slides))}
+      (for [note (map second slides)]
+        [:div.text-center.tracking-wider
+         {:style {:font-size (font-size enlargement)}} note])])])
+
 (defcomponent ^:endpoint panel [req ^:long enlargement ^:boolean shuff]
   (let [slides (slideshow/get-slideshow-slides-notes query-fn slideshow_id)
         slides (if shuff (shuffle slides) slides)
@@ -34,19 +52,9 @@
                  :shuff true}}]
      (if (empty? slides)
        [:div.p-6.text-xl "Empty"]
-       [:div {:class (format "grid grid-rows-2 grid-cols-%s" n)}
-        (map-indexed
-         (fn [i [[src2 src] note]]
-           [:div
-            [:img {:src (get-src src)
-                   :src2 (get-src src2)
-                   :onerror "fixSrc(event.target)"
-                   :hx-get "panel"
-                   :hx-vals {:enlargement (inc-mod enlargement)}}]
-            (when (pos? enlargement)
-              [:div.text-center.tracking-wider
-               {:style {:font-size (font-size enlargement)}} note])])
-         slides)])
+       [:div
+        (half-column enlargement (take n slides))
+        (half-column enlargement (drop n slides))])
      [:div {:style {:height "500px"}}]]))
 
 (defn ui-routes [{:keys [query-fn]}]
