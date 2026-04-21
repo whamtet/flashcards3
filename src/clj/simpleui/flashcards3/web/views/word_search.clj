b83a7efbf3030864e59ae2a2243f8abb4c45753f
Matthew Molloy <whamtet@gmail.com>
2026-04-21 21:05:14 +0700

    word search bugfix

--- src/clj/simpleui/flashcards3/web/views/word_search.clj
+++ src/clj/simpleui/flashcards3/web/views/word_search.clj
@@ -5,17 +5,6 @@
     [simpleui.flashcards3.web.views.components :as components]
     [simpleui.flashcards3.web.htmx :refer [page-htmx defcomponent]]))
 
-[:div.grid.grid-rows-10.grid-cols-10]
-[:div.border.grid-rows-11.grid-cols-11]
-[:div.grid-rows-12.grid-cols-12]
-[:div.grid-rows-13.grid-cols-13]
-[:div.grid-rows-14.grid-cols-14]
-[:div.grid-rows-15.grid-cols-15]
-[:div.grid-rows-16.grid-cols-16]
-[:div.grid-rows-17.grid-cols-17]
-[:div.grid-rows-18.grid-cols-18]
-[:div.grid-rows-19.grid-cols-19]
-[:div.grid-rows-20.grid-cols-20]
 (defcomponent panel [req ^:long grid-size]
   (let [grid-size (or grid-size 10)
         {:keys [grid words]} (word-search/ws-grid
@@ -23,7 +12,9 @@
                               slideshow_id
                               grid-size)]
     [:div
-     [:div {:class (format "grid grid-rows-%s grid-cols-%s border" grid-size grid-size)}
+     [:div {:class "grid border"
+            :style {:grid-template-rows (format "repeat(%s, minmax(0, 1fr))" grid-size)
+                    :grid-template-columns (format "repeat(%s, minmax(0, 1fr))" grid-size)}}
       (for [row grid col row]
         [:div.p-4 col])]
      [:div.flex.mt-2
